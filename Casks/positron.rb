b4bcf866e55460f4af2bf85f1bbee3910ae63f3d
Colin Rundel <rundel@gmail.com>
2024-06-27 13:25:56 -0400

    SHA and license removal

--- Casks/positron.rb
+++ Casks/positron.rb
@@ -3,8 +3,7 @@ cask "positron" do
   desc "A next-generation data science IDE"
   homepage "https://www.posit.co/"
   url "https://github.com/posit-dev/positron/releases/download/2024.06.1-27/Positron-2024.06.1-27.dmg"
-  sha256 ""
-  license "Elastic License 2.0"
+  sha256 "74ae5bbcb6818dc3c6bbd41ce7a6e32ebef8aa67daa42eafbb4e059ab5d68caa"
 
   livecheck do
     url :stable
