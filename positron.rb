508f74f168da9071f678212ed098791f92745269
Colin Rundel <rundel@gmail.com>
2024-06-27 13:12:46 -0400

    Fix

--- positron.rb
+++ positron.rb
@@ -1,4 +1,5 @@
-cask "rstudio" do
+cask "positron" do
+  name "Positron"
   desc "A next-generation data science IDE"
   homepage "https://www.posit.co/"
   url "https://github.com/posit-dev/positron/releases/download/2024.06.1-27/Positron-2024.06.1-27.dmg"
@@ -11,4 +12,5 @@ cask "rstudio" do
   end
 
   app "Positron.app"
+
 end
