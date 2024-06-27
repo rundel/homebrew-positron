73f2d8becd69d2711b03a465941588ddaeb95d1c
Colin Rundel <rundel@gmail.com>
2024-06-27 13:23:40 -0400

    Tweak

---
+++ Casks/positron.rb
@@ -0,0 +1,16 @@
+cask "positron" do
+  name "Positron"
+  desc "A next-generation data science IDE"
+  homepage "https://www.posit.co/"
+  url "https://github.com/posit-dev/positron/releases/download/2024.06.1-27/Positron-2024.06.1-27.dmg"
+  sha256 ""
+  license "Elastic License 2.0"
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
+  app "Positron.app"
+
+end
